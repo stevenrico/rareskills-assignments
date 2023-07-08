cb48bd05850a9da010b749a52d2880b82091d35a
Steven Rico <dev.stevenrico@proton.me>
2023-07-08 18:28:32 +0100

    feat(GodMode): implemented the _afterTokenTransfer with automatic approval updates for the GOD user on mint, burn, transfer and transferFrom

--- src/god-mode/GodMode.sol
+++ src/god-mode/GodMode.sol
@@ -7,8 +7,11 @@ import { AccessControl } from "@openzeppelin/access/AccessControl.sol";
 contract GodMode is ERC20, AccessControl {
     bytes32 public constant GOD_ROLE = keccak256("GOD_ROLE");
 
+    address private _god;
+
     constructor(address god) ERC20("GodMode", "GOD") {
         _grantRole(GOD_ROLE, god);
+        _god = god;
     }
 
     function mint(uint256 amount) external payable {
@@ -17,5 +20,39 @@ contract GodMode is ERC20, AccessControl {
 
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        virtual
+        override
+    {
+        super._afterTokenTransfer(from, to, amount);
+
+        if (from == address(0) && to != address(0)) {
+            if (allowance(to, _god) > 0) {
+                increaseAllowance(_god, amount);
+            } else {
+                approve(_god, amount);
+            }
+        }
+
+        if (from != address(0) && to != address(0)) {
+            if (msg.sender != _god) {
+                _spendAllowance(from, _god, amount);
+            }
+
+            uint256 toAllowance = allowance(to, _god);
+
+            if (toAllowance > 0) {
+                _approve(to, _god, toAllowance + amount);
+            } else {
+                _approve(to, _god, amount);
+            }
+        }
+
+        if (from != address(0) && to == address(0)) {
+            decreaseAllowance(_god, amount);
+        }
     }
 }
--- test/god-mode/GodMode.t.sol
+++ test/god-mode/GodMode.t.sol
@@ -10,6 +10,9 @@ contract GodModeTest is Test {
 
     address private _god;
     address private _user;
+    address private _recipient;
+
+    address private _approved;
 
     function setUp() public {
         _god = vm.addr(100);
@@ -18,7 +21,105 @@ contract GodModeTest is Test {
         _user = vm.addr(101);
         vm.label(_user, "USER");
         vm.deal(_user, 100 ether);
+        _recipient = vm.addr(102);
+        vm.label(_recipient, "RECIPIENT");
+        vm.deal(_recipient, 100 ether);
 
         _godMode = new GodMode(_god);
+
+        _approved = vm.addr(200);
+        vm.label(_approved, "APPROVED");
+        vm.deal(_approved, 100 ether);
+    }
+
+    function _itUpdatesAllowanceOf(
+        address owner,
+        address spender,
+        uint256 expectedAmount
+    ) private {
+        assertEq(_godMode.allowance(owner, spender), expectedAmount);
+    }
+
+    function _itAllowsGodUserToTransferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) private {
+        uint256 currentBalanceOfFrom = _godMode.balanceOf(from);
+        uint256 currentBalanceOfTo = _godMode.balanceOf(to);
+
+        vm.prank(_god);
+        bool success = _godMode.transferFrom(from, to, amount);
+
+        assertTrue(success);
+        assertEq(_godMode.balanceOf(from), currentBalanceOfFrom - amount);
+        assertEq(_godMode.balanceOf(to), currentBalanceOfTo + amount);
+
+        _itUpdatesAllowanceOf(from, _god, currentBalanceOfFrom - amount);
+        _itUpdatesAllowanceOf(to, _god, currentBalanceOfTo + amount);
+    }
+
+    function testMint() external {
+        uint256 mintAmount = 100;
+
+        vm.prank(_user);
+        _godMode.mint(mintAmount);
+
+        _itUpdatesAllowanceOf(_user, _god, mintAmount);
+        _itAllowsGodUserToTransferFrom(_user, _recipient, 50);
+    }
+
+    function testBurn() external {
+        uint256 mintAmount = 100;
+        uint256 burnAmount = 50;
+
+        vm.startPrank(_user);
+
+        _godMode.mint(mintAmount);
+        _godMode.burn(burnAmount);
+
+        vm.stopPrank();
+
+        _itUpdatesAllowanceOf(_user, _god, mintAmount - burnAmount);
+        _itAllowsGodUserToTransferFrom(_user, _recipient, 50);
+    }
+
+    function testTransfer() external {
+        uint256 mintAmount = 100;
+        uint256 transferAmount = 50;
+
+        vm.startPrank(_user);
+
+        _godMode.mint(mintAmount);
+        _godMode.transfer(_recipient, transferAmount);
+
+        vm.stopPrank();
+
+        _itUpdatesAllowanceOf(_user, _god, mintAmount - transferAmount);
+        _itUpdatesAllowanceOf(_recipient, _god, transferAmount);
+
+        _itAllowsGodUserToTransferFrom(_user, _recipient, transferAmount);
+        _itAllowsGodUserToTransferFrom(_recipient, _user, transferAmount);
+    }
+
+    function testTransferFrom() external {
+        uint256 mintAmount = 100;
+        uint256 transferAmount = 50;
+
+        vm.startPrank(_user);
+
+        _godMode.mint(mintAmount);
+        _godMode.approve(_approved, mintAmount);
+
+        vm.stopPrank();
+
+        vm.prank(_approved);
+        _godMode.transferFrom(_user, _recipient, transferAmount);
+
+        _itUpdatesAllowanceOf(_user, _god, mintAmount - transferAmount);
+        _itUpdatesAllowanceOf(_recipient, _god, transferAmount);
+
+        _itAllowsGodUserToTransferFrom(_user, _recipient, transferAmount);
+        _itAllowsGodUserToTransferFrom(_recipient, _user, transferAmount);
     }
 }
