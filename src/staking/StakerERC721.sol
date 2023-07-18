1c530469078dcc26010d7fd3be91d83ec1391b79
Steven Rico <dev.stevenrico@proton.me>
2023-07-18 15:45:25 +0100

    feat(StakerERC721): implemented royalties with the ERC-2981 standard; the contract can receive ETH and getBalance

--- src/staking/StakerERC721.sol
+++ src/staking/StakerERC721.sol
@@ -2,15 +2,36 @@
 pragma solidity ^0.8.19;
 
 import { ERC721 } from "@openzeppelin/token/ERC721/ERC721.sol";
+import { ERC2981 } from "@openzeppelin/token/common/ERC2981.sol";
 
-contract StakerERC721 is ERC721 {
+contract StakerERC721 is ERC721, ERC2981 {
     uint256 private _mintPrice;
 
     uint256 public constant MAX_SUPPLY = 20;
     uint256 private _totalSupply;
 
-    constructor(uint256 mintPrice) ERC721("Staker", "STKR") {
+    constructor(uint256 mintPrice, uint96 royaltyFee)
+        ERC721("Staker", "STKR")
+    {
         _mintPrice = mintPrice;
+
+        _setDefaultRoyalty(address(this), royaltyFee);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721, ERC2981)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    receive() external payable { }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 
     function mint() external payable {
@@ -19,9 +40,8 @@ contract StakerERC721 is ERC721 {
         );
 
         _totalSupply++;
-        
-        require(_totalSupply <= MAX_SUPPLY, "Staker: tokens are sold out");
 
+        require(_totalSupply <= MAX_SUPPLY, "Staker: tokens are sold out");
 
         _safeMint(msg.sender, _totalSupply);
     }
--- test/staking/StakerERC721.t.sol
+++ test/staking/StakerERC721.t.sol
@@ -12,12 +12,17 @@ contract StakerERC721Test is Test {
     address private _userOne;
     address private _userTwo;
 
+    address private _marketplace;
+
+    uint256 public constant MINT_PRICE = 1 ether;
+    uint96 public constant ROYALTY_FEE = 250;
+
     function setUp() public {
         _owner = vm.addr(100);
         vm.label(_owner, "OWNER");
         vm.deal(_owner, 100 ether);
 
-        _staker = new StakerERC721(1 ether);
+        _staker = new StakerERC721(MINT_PRICE, ROYALTY_FEE);
 
         _userOne = vm.addr(101);
         vm.label(_userOne, "USER ONE");
@@ -25,6 +30,10 @@ contract StakerERC721Test is Test {
         _userTwo = vm.addr(102);
         vm.label(_userTwo, "USER TWO");
         vm.deal(_userTwo, 100 ether);
+
+        _marketplace = vm.addr(200);
+        vm.label(_marketplace, "MARKETPLACE");
+        vm.deal(_marketplace, 100 ether);
     }
 
     function _itMintsAToken(address user, uint256 expectedAmount) private {
@@ -44,11 +53,11 @@ contract StakerERC721Test is Test {
         vm.startPrank(user);
 
         for (uint256 i = 1; i <= 19; i++) {
-            _staker.mint{ value: 1 ether}();
+            _staker.mint{ value: 1 ether }();
         }
-        
+
         vm.expectRevert("Staker: tokens are sold out");
-        _staker.mint{ value: 1 ether}();
+        _staker.mint{ value: 1 ether }();
 
         vm.stopPrank();
 
@@ -69,13 +78,44 @@ contract StakerERC721Test is Test {
         uint256 currentSupply = _staker.totalSupply();
 
         vm.prank(_userOne);
-        _staker.mint{ value: 1 ether}();
-    
+        _staker.mint{ value: 1 ether }();
+
         assertEq(_staker.totalSupply(), currentSupply + 1);
     }
 
     function testTotalSupply() external {
         _itStartsAtZero();
         _itIncrementsAfterMint();
+    }
+
+    function _itReturnsRoyaltyInfoForSale(address receiver, uint256 royalty)
+        private
+    {
+        assertEq(receiver, address(_staker));
+        assertEq(royalty, 0.025 ether);
+    }
+
+    function _itReceivesRoyaltiesFromMarketplace(
+        address receiver,
+        uint256 royalty
+    ) private {
+        vm.prank(_marketplace);
+        (bool success,) = address(receiver).call{ value: royalty }("");
+
+        assertTrue(success);
+        assertEq(_staker.getBalance(), 1.025 ether);
+    }
+
+    function testRoyalties() external {
+        vm.prank(_userOne);
+        _staker.mint{ value: 1 ether }();
+
+        uint256 salePrice = 1 ether;
+
+        vm.prank(_marketplace);
+        (address receiver, uint256 royalty) = _staker.royaltyInfo(1, salePrice);
+
+        _itReturnsRoyaltyInfoForSale(receiver, royalty);
+        _itReceivesRoyaltiesFromMarketplace(receiver, royalty);
     }
 }
