445a12e1ce2c4014293a8db65860972ba853b5a8
Steven Rico <dev.stevenrico@proton.me>
2023-08-03 10:35:53 +0100

    refactor(IPair): added burn function to interface

--- src/core/interfaces/IPair.sol
+++ src/core/interfaces/IPair.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 interface IPair {
-    // [Q] Why is the recipeient not included?
+    // [Q] Why is the recipient not included?
     event Mint(address indexed sender, uint256 amountA, uint256 amountB);
     event Burn(address indexed sender, address indexed recipient, uint256 amountA, uint256 amountB);
     event Swap(address indexed sender, address indexed recipient, uint256 amountAIn, uint256 amountAOut, uint256 amountBIn, uint256 amountBOut);
@@ -12,6 +12,7 @@ interface IPair {
     function getPriceCumulatives() external view returns (uint256 priceACumulative, uint256 priceBCumulative);
 
     function mint(address recipient) external returns (uint256 liquidtyTokens);
+    function burn(address recipient) external returns (uint256 amountA, uint256 amountB);
 
     function swap(uint256 amountAOut, uint256 amountBOut, address recipient) external;
 }
